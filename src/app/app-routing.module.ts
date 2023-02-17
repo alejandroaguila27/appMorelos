718efd60a57ef986436c72f00c3c7c503f204bac
Alex <alejandro.aguila.27@icloud.com>
2023-02-16 18:03:44 -0600

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SingupComponent } from './pages/singup/singup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PanelComponent } from './pages/panel/panel.component';
+
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: SingupComponent},
+  {path: 'panel', component: PanelComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PanelComponent } from './pages/panel/panel.component';
+import { SingupComponent } from './pages/singup/singup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    PanelComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
